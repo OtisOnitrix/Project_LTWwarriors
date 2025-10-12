5157a7ac86334a18b8d44ffa48b406201c068efa
OtisOnitrix <studyinmybraingeno@gmail.com>
2025-10-12 23:14:37 +0700

    Add project files.

---
+++ ProjectLTWwarriors/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjectLTWwarriors.Data;
+
+namespace ProjectLTWwarriors.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult LandingPage()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult SignUp()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult SignIn()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult ProductDetail(int id)
+        {
+            var product = ProductData.GetAllProducts().FirstOrDefault(p => p.Id == id);
+            if (product == null)
+                return HttpNotFound();
+
+            return View(product);
+
+            
+        }
+    }
+}   
---
+++ ProjectLTWwarriors/Data/ProductData.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ProjectLTWwarriors.Models;
+
+namespace ProjectLTWwarriors.Data
+{
+    public class ProductData
+    {
+        public static List<Product> GetAllProducts()
+        {
+            return new List<Product>
+            {
+                new Product
+                {
+                    Id = 1,
+                    Name = "iPhone 13 Pro Max",
+                    Price = 1099.99m,
+                    Storage = "128GB",
+                    Colors = new List<string> { "Silver", "Graphite", "Gold", "Sierra Blue" },
+                    ImageUrls = new List<string>
+                    {
+                        "~/Content/images/sony-xperia-1-iii-1.jpg",
+                        "~/Content/images/sony-xperia-1-iii-2.jpg",
+                        "~/Content/images/sony-xperia-1-iii-3.jpg"
+                    }
+                },
+                new Product
+                {
+                    Id = 2,
+                    Name = "Samsung Galaxy S21 Ultra",
+                    Price = 1199.99m,
+                    Storage = "256GB",
+                    Colors = new List<string> { "Phantom Black", "Phantom Silver" }
+                },
+                new Product
+                {
+                    Id = 3,
+                    Name = "Google Pixel 6 Pro",
+                    Price = 899.99m,
+                    Storage = "128GB",
+                    Colors = new List<string> { "Stormy Black", "Cloudy White", "Sorta Sunny" }
+                },
+                new Product
+                {
+                    Id = 4,
+                    Name = "OnePlus 9 Pro",
+                    Price = 969.99m,
+                    Storage = "256GB",
+                    Colors = new List<string> { "Morning Mist", "Pine Green", "Stellar Black" }
+                },
+                new Product
+                {
+                    Id = 5,
+                    Name = "Sony Xperia 1 III",
+                    Price = 1299.99m,
+                    Storage = "256GB",
+                    Colors = new List<string> { "Frosted Black", "Frosted Purple" }
+                }
+            };
+        }
+    }
+}
